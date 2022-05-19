5b7c09b4bbfa290bc1c9790acf2d58970be6b92d
Nikolaus Rosenmayr <nr@firecoder.com>
2022-05-19 11:07:19 +0200

    chore(build): basic npm project setup
    define dev dependencies and make some source build.
    This is the basic project setup.
    
    Signed-off-by: Nikolaus Rosenmayr <nr@firecoder.com>

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import type { UserConfigExport } from "vite";
+
+import * as path from "path";
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pkg = require("./package.json") as { name: string, main: string, dependencies: Record<string, string> };
+
+// https://vitejs.dev/config/
+export const BaseConfig: UserConfigExport = {
+    plugins: [],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url))
+        }
+    },
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/main.ts"),
+            // package name is in kebab-case, for the name of the browser global, we want camelCase
+            name: pkg.name
+                .replace(/-./g, ((w) => w[1].toUpperCase()))
+                .replace(/^./, ((w) => w[0].toUpperCase()))
+            ,
+            fileName: (format) => path.basename(pkg.main),
+            formats: ["es"],
+        },
+        rollupoptions: {
+            external: [...Object.getOwnPropertyNames(pkg.dependencies), "tslib"],
+        }
+    },
+};
+
+export default defineConfig(BaseConfig)
