351aa26bc098f67c1611c5b5d44c9a440a40d169
Nikolaus Rosenmayr <nr@firecoder.com>
2022-06-16 00:16:50 +0200

    feat: introduce life-cycle function mapper
    The life-cycle function names of the legacy Options API differs from
    the new Composition API. So, a mapper is introduced. Fortunately, the
    function definitions seem to be backwards compatible.
    
    The mapping object uses the old names as property names and defines
    each as a function to call the new Composition API functions.
    
    In order to keep the Composition API functions mockable, the
    properties of the mapper are not directly assigned from the target
    functions. Instead, wrapper functions are used to call the target
    functions at runtime. Then any mocked or stubbed Composition API
    function will still be used and tested.
    
    Although this might seem a slow down at runtime, I am very confident
    that JIT compilers will optimise and that there will be no penalty.
    
    Signed-off-by: Nikolaus Rosenmayr <nr@firecoder.com>

---
+++ src/decorator/life-cycle-hooks.ts
@@ -0,0 +1,101 @@
+// inspired by and much taken from here:
+// https://github.com/vuejs/vue-class-component/blob/16433462b40aefecc030919623f17b0ec9afe61c/src/component.ts#L24
+
+import type { ComponentInternalInstance } from "vue";
+import type { DebuggerHook, ErrorCapturedHook } from "../vue";
+
+import { CompositionApi } from "../vue";
+
+export type TLifeCycleHookRegisterFunction =
+    | (() => undefined)
+    | typeof CompositionApi.onBeforeMount
+    | typeof CompositionApi.onMounted
+    | typeof CompositionApi.onBeforeUnmount
+    | typeof CompositionApi.onBeforeUnmount
+    | typeof CompositionApi.onUnmounted
+    | typeof CompositionApi.onBeforeUpdate
+    | typeof CompositionApi.onUpdated
+    | typeof CompositionApi.onActivated
+    | typeof CompositionApi.onDeactivated
+    | typeof CompositionApi.onRenderTriggered
+    | typeof CompositionApi.onErrorCaptured
+    | typeof CompositionApi.onServerPrefetch
+;
+
+/**
+ * Provides mapping between legacy life cycle hooks and new Composition API functions.
+ *
+ * <p>
+ *     The Composition API functions are not directly bound to new names but wrapped with a function. Thus, any
+ *     mocked API function will be used instead af od the original function. This way, the tests are really able to
+ *     check, whether the Composition API functions are called properly.
+ * </p>
+ */
+export const $lifeCycleHookRegisterFunctions: {
+    [hookName: string]: TLifeCycleHookRegisterFunction;
+} = {
+    beforeCreate: () => undefined,
+    created: () => undefined,
+
+    beforeMount: function beforeMount(hook: () => unknown, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onBeforeMount(hook, target);
+    },
+
+    mounted: function mounted(hook: () => unknown, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onMounted(hook, target);
+    },
+
+    beforeDestroy: function beforeDestroy(hook: () => unknown, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onBeforeUnmount(hook, target);
+    },
+
+    beforeUnMount: function beforeUnMount(hook: () => unknown, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onBeforeUnmount(hook, target);
+    },
+
+    destroyed: function destroyed(hook: () => unknown, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onUnmounted(hook, target);
+    },
+
+    unmounted: function unmounted(hook: () => unknown, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onUnmounted(hook, target);
+    },
+
+    beforeUpdate: function beforeUpdate(hook: () => unknown, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onBeforeUpdate(hook, target);
+    },
+
+    updated: function updated(hook: () => unknown, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onUpdated(hook, target);
+    },
+
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    activated: function activated(hook: Function, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onActivated(hook, target);
+    },
+
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    deactivated: function deactivated(hook: Function, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onDeactivated(hook, target);
+    },
+
+    render: function render(hook: DebuggerHook, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onRenderTriggered(hook, target);
+    },
+
+    errorCaptured: function errorCaptured<TError = Error>(
+        hook: ErrorCapturedHook<TError>, target?: ComponentInternalInstance | null,
+    ) {
+        return CompositionApi.onErrorCaptured(hook, target);
+    },
+
+    serverPrefetch: function serverPrefetch(hook: () => unknown, target?: ComponentInternalInstance | null) {
+        return CompositionApi.onServerPrefetch(hook, target);
+    },
+};
+
+export const $lifeCycleHookNames = Object.getOwnPropertyNames($lifeCycleHookRegisterFunctions);
+export const $internalHookNames = [
+    ...$lifeCycleHookNames,
+    "data",
+];
