0580b78afaf35720fd0f4751ebbb9d06013ba196
Nikolaus Rosenmayr <nr@firecoder.com>
2022-06-18 01:36:29 +0200

    feat: map "computed" option API to composition API
    A new utility function created proper composition API calls from the
    `computed` component options.
    
    In order to make them reactive, the whole Vue instance is wrapped
    with a reactive Proxy wrapper. If a (computed) property is set via the
    wrapper, all dependent other properties will be updated. This way,
    the data values do not need to be made reactive explicitly.
    In previous vue-class-component, all value properties are added as
    mixins of `data`. Since mixins are not supported by Vue anymore, a
    different mechanism is used now.
    
    This utility will be used by the component decorator to setup the
    newly created component.
    
    Signed-off-by: Nikolaus Rosenmayr <nr@firecoder.com>

--- src/decorator/init-component.ts
+++ src/decorator/init-component.ts
@@ -1,5 +1,7 @@
+import type { Ref } from "vue";
 import type { CompatibleComponentOptions, Vue } from "../vue";
 
+import { reactive } from "vue";
 import { CompositionApi } from "../vue";
 import { $internalHookNames } from "./life-cycle-hooks";
 
@@ -52,6 +54,103 @@ export function applyInjectsOnInstance(
                 instance[propName] = CompositionApi.inject(fromProvidedKey, defaultValue, false);
             }
         }
+    }
+}
+
+/**
+ * Defines a property that is a Vue reactive reference.
+ *
+ * <p>
+ *     This project is about best supporting TypeScript classes. Computed properties on instances usually are defined
+ *     in the class but are not expected to have an accessor property of {@code .value}. So, the reference property
+ *     applied where will be unwrapped automatically.
+ * </p>
+ *
+ * @param instance the instance to define the new property.
+ * @param propertyName the name of the new property
+ * @param vueReference the Vue reactive reference to use for reading and setting values
+ * @param hasSetter set to {@code true} to apply a setter as well. Otherwise, no setter will be created.
+ * @throws Error if the new property is already defined on the instance with type function.
+ * @see https://vuejs.org/guide/essentials/reactivity-fundamentals.html#ref-unwrapping-in-reactive-objects
+ */
+export function definePropertyFromRef(
+    instance: Vue,
+    propertyName: string,
+    vueReference: Ref,
+    hasSetter?: boolean,
+): void {
+    if (
+        vueReference && instance && typeof instance === "object" &&
+        propertyName && typeof propertyName === "string"
+    ) {
+        if (typeof instance[propertyName] === "function") {
+            throw new Error(`The property ${propertyName} is already defined as a member function.`);
+        }
+
+        Object.defineProperty(instance, propertyName, {
+            get: () => vueReference.value,
+            set: (hasSetter ? (newValue) => {
+                vueReference.value = newValue;
+            } : undefined),
+        });
+    }
+}
+
+
+export function registerComputedValues(
+    instance: Vue,
+    computedValues: CompatibleComponentOptions<Vue>["computed"],
+): void {
+    if (instance && typeof computedValues === "object") {
+        // wrap the instance inside a reactive to enable propagation of value changes.
+        // see: https://github.com/vuejs/core/issues/6130
+        //
+        // In order to make the computed values reactive, the whole instance is wrapped
+        // with a reactive Proxy wrapper. If a (computed) property is set via this
+        // wrapper, all dependent other properties will be updated. This way,
+        // the value based properties do not need to be made reactive explicitly.
+        // In previous vue-class-component, all value properties are added as
+        // mixins of `data`. Since mixins are not supported by Vue anymore, a
+        // different mechanism is used now.
+        const reactiveInstance = reactive(instance);
+
+        // add computed properties but assign "this" context to instance
+        Object.getOwnPropertyNames(computedValues || {})
+            .filter(isNotInternalHookName)
+            .forEach((key) => {
+
+                const computedSpec = computedValues[key];
+                if (typeof computedSpec === "function") {
+                    definePropertyFromRef(
+                        instance,
+                        key,
+                        CompositionApi.computed(computedSpec.bind(reactiveInstance)),
+                        false,
+                    );
+
+                } else if (
+                    computedSpec && typeof computedSpec === "object" &&
+                    typeof computedSpec.get === "function" &&
+                    typeof computedSpec.set === "function"
+                ) {
+                    // Vue 3 requires every setter to have a companion getter. A computed value without a getter
+                    // is invalid!
+                    definePropertyFromRef(
+                        instance,
+                        key,
+                        CompositionApi.computed({
+                            get: computedSpec.get.bind(reactiveInstance),
+                            set: computedSpec.set.bind(reactiveInstance),
+                        }),
+                        true,
+                    );
+
+                } else {
+                    const jsonDebug = JSON.stringify(computedSpec, undefined, 4);
+                    CompositionApi.warn(`Invalid "computed" specification for property ${key}: ${jsonDebug}`);
+                }
+            })
+        ;
     }
 }
 
---
+++ test/decorator/init-component-computed.spec.ts
@@ -0,0 +1,139 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import type { Vue } from "../../src/vue";
+
+import {computed, reactive, triggerRef} from "vue";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import * as sinon from "sinon";
+
+import { registerComputedValues } from "../../src/decorator/init-component";
+import { CompositionApi } from "../../src/vue/composition-api";
+
+
+afterEach(() => {
+    sinon.restore();
+});
+
+describe("registerComputedValues(): Register computed properties", () => {
+    it("Use a function as computed property", () => {
+        const computedProperties = {
+            meaning: () => 42,
+        };
+
+        const mockedApi = sinon.mock(CompositionApi);
+        const mockedComputed = mockedApi.expects("computed");
+        mockedComputed
+            .once()
+            .returns(computed(computedProperties.meaning))
+        ;
+
+        const instance = {} as Vue;
+        registerComputedValues(instance, computedProperties);
+
+        mockedComputed.verify();
+        expect(instance["meaning"]).toBeTypeOf("number");
+        expect(instance["meaning"]).toEqual(computedProperties.meaning());
+    });
+
+    it("Computed property with only a getter function fails when setting a value", () => {
+        const computedProperties = {
+            failOnSetter: () => "fail on setter",
+        };
+
+        const mockedApi = sinon.mock(CompositionApi);
+        const mockedComputed = mockedApi.expects("computed");
+        mockedComputed
+            .once()
+            .returns(computed(computedProperties.failOnSetter))
+        ;
+
+        const instance = {} as Vue;
+        registerComputedValues(instance, computedProperties);
+
+        mockedComputed.verify();
+        expect(instance["failOnSetter"]).toBeTypeOf("string");
+        expect(instance["failOnSetter"]).toEqual(computedProperties.failOnSetter());
+        expect(() => { instance["failOnSetter"] = "this will fail"; }).toThrowError();
+    });
+
+    it("Computed property with getter and setter function", () => {
+        const instance = { testValue: "starting" as unknown, test: undefined as unknown };
+        const computedProperties = {
+            test: {
+                get: function (this: typeof instance) {
+                    return this.testValue;
+                },
+                set: function (this: typeof instance, newValue: unknown) {
+                    this.testValue = newValue;
+                },
+            },
+        };
+
+        registerComputedValues(instance, computedProperties);
+        expect(instance.test).toBeTypeOf("string");
+        expect(instance.test).toEqual("starting");
+
+        instance.test = "new Value";
+        expect(instance.testValue).toBeTypeOf("string");
+        expect(instance.testValue).toEqual(instance.testValue);
+        expect(instance["test"]).toEqual("new Value");
+
+        instance["test"] = 934;
+        expect(instance.testValue).toBeTypeOf("number");
+        expect(instance.testValue).toEqual(934);
+    });
+
+    it("Computed property is cached but will change with setter", () => {
+        const instance = { rawValue: "starting" as unknown, cachedValue: undefined as unknown };
+        const computedProperties = {
+            cachedValue: {
+                get: function (this: typeof instance) {
+                    return this.rawValue;
+                },
+                set: function (this: typeof instance, newValue: unknown) {
+                    this.rawValue = newValue;
+                },
+            },
+        };
+
+        registerComputedValues(instance, computedProperties);
+        expect(instance.cachedValue).toBeTypeOf("string");
+        expect(instance.cachedValue).toEqual("starting");
+
+        instance.cachedValue = "new Value";
+        expect(instance.cachedValue).toEqual("new Value");
+    });
+
+    it("Apply multiple computed", () => {
+        const helloPrefix = "Hello ";
+        const instance: { [key: string]: unknown } = {
+            rawValue: "starting" as unknown,
+            cachedValue: undefined as unknown,
+        };
+        const computedProperties = {
+            cachedValue: {
+                get: function (this: typeof instance) {
+                    return this.rawValue;
+                },
+                set: function (this: typeof instance, newValue: unknown) {
+                    this.rawValue = newValue;
+                },
+            },
+            valueWithHello: function (this: typeof instance) { return helloPrefix + this.rawValue; },
+            valueWithHelloFromCached: () => helloPrefix + instance.cachedValue,
+            valueFromFixedInstance: () => helloPrefix + instance.rawValue,
+        };
+
+        registerComputedValues(instance, computedProperties);
+        expect(instance.valueWithHello).toEqual(helloPrefix + "starting");
+        expect(instance.valueWithHelloFromCached).toEqual(helloPrefix + "starting");
+        expect(instance.valueFromFixedInstance).toEqual(helloPrefix + "starting");
+
+        instance.cachedValue = "new Value";
+        expect(instance.rawValue).toEqual("new Value");
+        expect(instance.valueWithHello).toEqual(helloPrefix + "new Value");
+        expect(instance.valueWithHelloFromCached).toEqual(helloPrefix + "new Value");
+
+        // the cache is not invalidated in the computed ref, so the new value is not reflected.
+        expect(instance.valueFromFixedInstance).toEqual(helloPrefix + "starting");
+    });
+});
